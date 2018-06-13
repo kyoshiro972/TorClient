ae69d7d52f1621653c21b88862d6980df8c6383f
Hafizh <hafizh@hafizhs-macbook-pro.local>
2018-06-13 10:45:56 +0800

    Initial Commit

---
+++ TorClient/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TorClient
+//
+//  Created by Hafizh on 13/06/18.
+//  Copyright © 2018 Hafizh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
